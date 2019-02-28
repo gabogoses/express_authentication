d5a1d838c0e822ac58a0ed0eba48eb98ccd28154
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-02-28 23:34:01 +0100

    Layout added, Login & Register views added

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 // Login Page
-router.get("/login", (req, res) => res.send("Login 🤖"));
+router.get("/login", (req, res) => res.render("Login"));
 
 // Register Page
-router.get("/register", (req, res) => res.send("Register 👽"));
+router.get("/register", (req, res) => res.render("Register"));
 
 module.exports = router;
