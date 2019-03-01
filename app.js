5356eeef08096a91da8e047242d8b20b555fa49d
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-03-01 02:09:19 +0100

    Passport added

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
+
+// DB Config
+const db = require("./config/keys").MongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+  session({
+    secret: "keyboard dog",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // Routes
 app.use("/", require("./routes/index"));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://gabo:TSXwICiqPdRx7msy@authenticate-xa05c.mongodb.net/auth?retryWrites=true"
+};
