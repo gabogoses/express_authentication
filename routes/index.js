8f36cdf859ede87b68b27e983d8872062176c9c8
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-03-02 13:00:31 +0100

    views updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { ensureAuthenticated } = require("../config/auth");
 
 // Welcome Page
-router.get("/", (req, res) => res.render("welcome 🤖"));
+router.get("/", (req, res) => res.render("welcome"));
 
 // Dashboard
 router.get("/dashboard", ensureAuthenticated, (req, res) =>
